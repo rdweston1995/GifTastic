3c4fc679e760ff30f1644b8f0ccd88281c7a3f01
Robert <robert.d.weston@gmail.com>
2019-05-01 15:55:15 -0700

    Basic site done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,43 @@
 $(document).ready(function () {
-    var topics = ["Jon Snow", "Theon Greyjoy", "Brandon Stark", "Ser Davos", "Tormund Giantsbane", 'Sandor "The Hound" Clegene', 'Beric Dondarion'];
+    var topics = ["Jon Snow", "Theon Greyjoy", "Brandon Stark", "Ser Davos", "Tormund Giantsbane", 'Sandor Clegene', 'Beric Dondarion', 'Gandalf', 'cats'];
     var apiKey = "https://api.giphy.com/v1/gifs/random?api_key=RrgqNg9Dm83UXhvKWjvgP434CbrzUhgi";
+    var buttonNum = 0;
 
     for (var i = 0; i < topics.length; i++) {
-        $(".buttonList").append("<button class='btn btn-primary'>" + topics[i] + "</button>");
+        //$(".buttonList").append("<button class='btn btn-primary'>" + topics[i] + "</button>");
+        $(".buttonList").append("<button class='btn btn-primary' id='button" + buttonNum + "'>" + topics[i] + "</button>");
+        buttonNum++;
+        console.log($("#button" + i).text());
     }
 
     $("button").on("click", function(){
+        /*
         $.ajax({
-            url: apiKey + "&tag=" + $(this).text,
+            url: apiKey + "&tag=" + $(this).text(),
             method: 'GET'
-        }).then(function(data){
-            console.log(data);
+        }).then(function(response){
+            console.log($(this).text());
+            console.log(response);
+
+            $(".container").prepend("<img class='gif' src='" + response.data.image_original_url + "'>")
+            $("img").attr("alt", $(this).text + " image");
         });
+        */ 
+        //console.log($(this).text());
+        for(var i = 0; i < 10; i++){
+            $.ajax({
+                url: apiKey + "&tag=" + $(this).text(),
+                method: 'GET'
+            }).then(function(response){
+                $(".container").prepend("<img class='gif' id='gif" + i + "' src='" + response.data.image_original_url + "'>");
+                $("img").attr("alt", $(this).text() + " image");
+            });
+        }
+    });
+
+    $("aside").append("<button class='clear'>CLEAR</button>");
+    $(".clear").on("click", function(){
+        $(".container").empty();
     });
 });
 
