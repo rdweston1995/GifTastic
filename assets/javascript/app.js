42742a2a580ce3a6f7bde2d410f711d9cab34c01
Robert <46203504+rdweston1995@users.noreply.github.com>
2019-05-04 13:40:10 -0700

    Finished app just need more style work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,41 @@
 $(document).ready(function () {
-    var topics = ["Jon Snow", "Theon Greyjoy", "Tormund Giantsbane", 'Sandor Clegene', 'Beric Dondarion', 'Gandalf', 'cats', 'Owen Wilson'];
+    //var topics = ["Jon Snow", "Theon Greyjoy", "Tormund Giantsbane", 'Sandor Clegene', 'Beric Dondarion', 'Gandalf', 'cats', 'Owen Wilson'];
+    var topics = ["Owen Wilson", "Owen Wilson", "Owen Wilson", "Owen Wilson", "Owen Wilson", "Owen Wilson", "Owen Wilson"];
     var apiKEY = "&api_key=VwVG9Ri3ZO8eWDbFxQj1aR46gxpi88cV&limit=10";
 
     var buttonNum = 0;
 
+    //For loop to initially add the buttons to the page for each element in the topics array
     for (var i = 0; i < topics.length; i++) {
         $(".buttonList").append("<button class='characterBtn btn btn-primary' id='button" + buttonNum + "'>" + topics[i] + "</button>");
+        //Adding on click events to each new button
         btnClick();
+        //Iterating buttonNum 
         buttonNum++;   
     }
 
     //On click for adding another character button
     $("#addButton").on("click", function(){
+        //Adding the new button to the buttonList class
         $(".buttonList").append("<button class='characterBtn btn btn-primary' id='button" + buttonNum + "'>" + $("#newButton").val() + "</button>");
+        //Adds to the new topic to the topics array
+        topics.push($("#newButton").val());
+        //Run the function to add an on click event for the new button
         btnClick();
+        //Iterate buttonNum
         buttonNum++;
+        //Clear the value of the newButton id after the new topic has been added
         $("#newButton").val("");
     });
 
 
-    //$("aside").append("<button class='clear'>CLEAR</button>");
+    //On click for clearing the gifs off the page and clearing the console
     $(".clear").on("click", function(){
         $(".container").empty();
         console.clear();
     });
 
-
+    //Adds on click function to the buttons at the top of the page 
     function btnClick() {
         $(".container").empty();
         //When called only retrieves one gif
@@ -51,14 +61,16 @@ $(document).ready(function () {
                     //$("img").attr("alt", removeSpace + " gif");
 
                     $(".container").append("<p class='divContainers' id='div" + i + "'></p");
-                    $("#div" + i).prepend("<img class='gif' src='" + response.data[i].images.original_still.url + "' data-still='" + response.data[i].images.original_still.url + "' data-animate='" + response.data[i].images.original.url + "' data-state='still'>");
                     $("#div" + i).prepend("<p class='rating'>" + response.data[i].rating + "</p>");
+                    $("#div" + i).prepend("<img class='gif" + i + "' src='" + response.data[i].images.original_still.url + "' data-still='" + response.data[i].images.original_still.url + "' data-animate='" + response.data[i].images.original.url + "' data-state='still'>");
+                    
 
 
-                    $(".gif").on('click', function () {
+                    $(".gif" + i).on('click', function () {
                         var state = $(this).attr("data-state");
     
                         if (state === "still") {
+                            console.log("test");
                             $(this).attr("src", $(this).attr("data-animate"));
                             $(this).attr("data-state", "animate");
                         } else {
